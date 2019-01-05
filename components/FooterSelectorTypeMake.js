ae61ce7d1913f836934fba5b0e8dc05db53c0142
Hung Do <hung96bk@gmail.com>
2019-01-05 10:52:56 +0700

    add tooltips

--- components/FooterSelectorTypeMake.js
+++ components/FooterSelectorTypeMake.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 import Link from 'next/link'
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
+const tooltip = (
+    <Tooltip id="tooltip">
+        <strong>Holy guacamole!</strong>
+    </Tooltip>
+);
 export default class FooterSelectorTypeMake extends Component {
     render() {
         return (
             <Link href={this.props.href}>
-                <a className="make-type-icon" href="javascript:void(0)" title={String(this.props.title).toUpperCase()}>
-                    <i className={this.props.icon}> </i>
-                </a>
+                    <a className="make-type-icon" href="javascript:void(0)">
+                        <OverlayTrigger placement="top" overlay={<Tooltip id="tooltip" delayShow="0.1s" >{String(this.props.title).toUpperCase()}</Tooltip>}>
+                            <i className={this.props.icon}> </i>
+                        </OverlayTrigger>
+                    </a>
             </Link>
         )
     }
